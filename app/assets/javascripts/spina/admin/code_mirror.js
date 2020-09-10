130e313a0e3f1fb6d095ece826fb96c21bc9e841
Santiago Sandoval <santiago_st_95@hotmail.com>
2020-09-10 14:32:20 -0500

    code-mirror commented

--- app/assets/javascripts/spina/admin/code_mirror.js
+++ app/assets/javascripts/spina/admin/code_mirror.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
-  $('.code-mirror').each(function() {
-    CodeMirror.fromTextArea($(this).get(0), {
-      lineNumbers: true,
-      matchBrackets: true,
-      styleActiveLine: true,
-      theme: 'ambiance',
-      mode: 'text/xml'
-    });
-  });
-});
+// $(document).ready(function(){
+//   $('.code-mirror').each(function() {
+//     CodeMirror.fromTextArea($(this).get(0), {
+//       lineNumbers: true,
+//       matchBrackets: true,
+//       styleActiveLine: true,
+//       theme: 'ambiance',
+//       mode: 'text/xml'
+//     });
+//   });
+// });
